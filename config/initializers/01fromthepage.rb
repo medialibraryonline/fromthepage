2283769dda89afd55627713c3ea29744bd1f1be7
raffaele messuti <raffaele@atomotic.com>
2019-10-28 12:22:07 +0100

    custom skin

--- config/initializers/01fromthepage.rb
+++ config/initializers/01fromthepage.rb
@@ -1,7 +1,7 @@
 ADMIN_EMAILS = 'saracarl@gmail.com, benwbrum@gmail.com'
 SENDING_EMAIL_ADDRESS = 'support@fromthepage.com'
 SMTP_ENABLED = false
-USE_PNG_LOGO = false
+USE_PNG_LOGO = true
 GUEST_DEED_COUNT = 3
 GUEST_TRANSCRIPTION_ENABLED = false
 FACEBOOK_PIXEL_ID = ''
